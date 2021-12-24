9547846d1d3b9d66243a183b7bd7dac6ac31ef9c
SurajMali <76955549+SurajMali002@users.noreply.github.com>
2021-12-24 17:15:39 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC7MeZR1ruHZxsIXc1P8A99xvCJNF0vTfY",
+    authDomain: "storytellerapp-2c741.firebaseapp.com",
+    databaseURL: "https://storytellerapp-2c741-default-rtdb.firebaseio.com",
+    projectId: "storytellerapp-2c741",
+    storageBucket: "storytellerapp-2c741.appspot.com",
+    messagingSenderId: "1038008634416",
+    appId: "1:1038008634416:web:a5af3649bb44ccc5c231f5"
+  };
